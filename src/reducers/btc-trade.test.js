784afdd07465e4341be6917e225e51002f73c78a
pawankeer <pawandeep.keer@gmail.com>
2018-05-06 01:59:02 -0400

    testing btc-trade reducer

---
+++ src/reducers/btc-trade.test.js
@@ -0,0 +1,37 @@
+import expect from 'expect';
+import btcTrade from './btc-trade';
+
+
+describe('test btc-trade reducer', () => {
+  it('should return same state for action UPDATE_BTC_REQUEST', () => {
+    const state = {};
+    const action = { type: 'UPDATE_BTC_REQUEST' };
+    const newBTCTrade = btcTrade(state, action);
+    expect(newBTCTrade).toEqual(state);
+  });
+
+  it('should return same state for action UPDATE_BTC_FAILURE', () => {
+    const state = {};
+    const expectedState = { error: true, errorMessage: 'something went wrong' };
+    const action = { type: 'UPDATE_BTC_FAILURE', error: 'something went wrong' };
+    const newBTCTrade = btcTrade(state, action);
+    expect(newBTCTrade).toEqual(expectedState);
+  });
+
+  it('should return same state for action UPDATE_BTC_SUCCESS', () => {
+    const state = {};
+    const expectedState = {
+      error: false, errorMessage: undefined, rate: 100.00, usd: 10.00, btc: 0.10,
+    };
+    const action = { type: 'UPDATE_BTC_SUCCESS', rate: '100', usd: '10' };
+    const newBTCTrade = btcTrade(state, action);
+    expect(newBTCTrade).toEqual(expectedState);
+  });
+
+  it('should return same state for action default', () => {
+    const state = {};
+    const action = { type: 'default' };
+    const newBTCTrade = btcTrade(state, action);
+    expect(newBTCTrade).toEqual(state);
+  });
+});
